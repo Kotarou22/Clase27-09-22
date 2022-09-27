4a2e11b940590ec87a9d906907342218cdb837de
MatiasM <mi.munozc@duocuc.cl>
2022-09-27 16:24:36 -0400

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    loadChildren: () => import('./paginas/pokemones/pokemones.module').then(m =>m.PokemonesPageModule)
+  },
+  {
+    path: ':nombrePokemon',
+    loadChildren: () => import('./paginas/pokemon/pokemon.module').then(m => m.PokemonPageModule)
+  }
 ];
 
 @NgModule({
